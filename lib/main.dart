429dc02f78dbd5e13834f5b34deac220b0116f4d
Felipe Ferreira <felipe.flores@tbdc.com.br>
2023-12-20 18:59:55 -0300

    refactor: main app structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,7 @@ import 'package:expense_tracker/expenses_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ExpensesApp());
+  runApp(const MaterialApp(
+    home: ExpensesApp(),
+  ));
 }
