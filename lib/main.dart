17662293e421b25b589251f95e2dfc67f8c5f87d
Felipe Ferreira <felipe.flores@tbdc.com.br>
2023-12-21 16:42:02 -0300

    feat: added dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,29 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(MaterialApp(
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kDarkColorScheme,
+      cardTheme: const CardTheme().copyWith(
+        color: kDarkColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        ),
+      ),
+    ),
     theme: ThemeData().copyWith(
       colorScheme: kColorScheme,
       appBarTheme: const AppBarTheme().copyWith(
@@ -16,7 +37,10 @@ void main() {
       ),
       cardTheme: const CardTheme().copyWith(
         color: kColorScheme.secondaryContainer,
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
@@ -36,6 +60,7 @@ void main() {
             ),
           ),
     ),
+    themeMode: ThemeMode.system,
     home: const ExpensesApp(),
   ));
 }
